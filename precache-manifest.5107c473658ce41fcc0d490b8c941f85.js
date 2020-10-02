e30f8a341e08e77cf49388d2c5141a91d8f05c09
dyoonar <71246827+dyoonar@users.noreply.github.com>
2020-10-02 08:27:33 +0700

    Add files via upload

---
+++ precache-manifest.5107c473658ce41fcc0d490b8c941f85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c438240d6ae185aa90e63b5dbc314c48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5e5909e03f456f48655",
+    "url": "/static/css/main.d442f581.chunk.css"
+  },
+  {
+    "revision": "b036694bcdee6885d641",
+    "url": "/static/js/2.25cee438.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.25cee438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5e5909e03f456f48655",
+    "url": "/static/js/main.7080d069.chunk.js"
+  },
+  {
+    "revision": "94add0f1748447182057",
+    "url": "/static/js/runtime-main.d7ab499f.js"
+  }
+]);
